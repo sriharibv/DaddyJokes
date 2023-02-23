e05538998fe177122c5b611a95d7075c542f3d4e
srihariharanbv <srihariharanbv@gmail.com>
2023-02-24 00:17:22 +0530

    Modified the table

---
+++ Data/Datatable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace DaddyJokes.Data
+{
+    public class Datatable<T>
+    {
+
+        [JsonPropertyName("current_page")]
+        public int PageNumber { get; set; }
+
+        [JsonPropertyName("results")]
+        public IList<T> Items { get; set; }
+
+        [JsonPropertyName("total_jokes")]
+        public int TotalItems { get; set; }
+
+        [JsonPropertyName("total_pages")]
+        public int TotalPages { get; set; }
+    }
+}
+
---
+++ Helpers/WordCountHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace DaddyJokes.Helpers
+{
+    public static class WordCountHelper
+    {
+        public static int Count(string x)
+        {
+            //Trim whitespace from beginning and end of string
+            x = x.Trim();
+
+            if (string.IsNullOrEmpty(x))
+                return 0;
+
+            //Ensure there is only one space between each word in the passed string
+            while (x.Contains("  "))
+                x = x.Replace("  ", " ");
+
+            //Count the words
+            return x.Split(' ').Length;
+        }
+    }
+}
+
